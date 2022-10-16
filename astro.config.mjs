8921503e15a4f3b312e45b425e131bd5e8ec25d2
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-10-16 17:27:57 +0700

    Latest update 16/10/2022 17:27PM

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwind from '@astrojs/tailwind';
 import { VitePWA } from 'vite-plugin-pwa';
 import sitemap from '@astrojs/sitemap';
 import node from '@astrojs/node';
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: deno(),
     integrations: [
         svelte(),
         tailwind(),
